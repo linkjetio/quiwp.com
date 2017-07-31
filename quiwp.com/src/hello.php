ef3f73e3e8ae31d18fb99d3a0098ab16860b14ac
ashulogic <ashwinisbhagwat@yahoo.com>
2017-07-31 18:12:08 +0530

    First commit

--- quiwp.com/src/hello.php
+++ quiwp.com/src/hello.php
@@ -1,14 +1,14 @@
 <?php
 	include('format.php');
-	system('  scp lamp123.php ljadminvm@192.168.0.5:lamp123.php');
+	system('scp lamp123.php ljadminvm@192.168.0.5:lamp123.php');
 	system('date');
 	echo "\n\n";
 	echo colorize("Your File Has Been Succesfully Copied...", "SUCCESS");
 	echo "\n\n";
-	system(' scp mydata.sql ljadminvm@192.168.0.5:mydata.sql');
+	system('scp mydata.sql ljadminvm@192.168.0.5:mydata.sql');
 	system('date');
 	echo "\n\n";
-	echo colorize(" Your File Has Been Succesfully Copied...", "SUCCESS");
+	echo colorize("Your File Has Been Succesfully Copied...", "SUCCESS");
 	echo "\n\n";
 	system('ssh ljadminvm@192.168.0.5');
 ?>
